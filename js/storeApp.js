ec6868670f205ab0e9cd1bd614ff5291ecbc9aaa
yamahatake <yamaaug@gmail.com>
2015-03-17 23:00:36 -0300

    .

--- js/storeApp.js
+++ js/storeApp.js
@@ -10,17 +10,18 @@
 	    // disable IE ajax request caching
 	    $httpProvider.defaults.headers.get['If-Modified-Since'] = '0';
 	    //.....here proceed with your routes
+	    app.controller('ToolsController',function($scope, $http){
+			$http.get('js/ferramentas.json').then(function(res){
+			    $scope.ferramentaitens = res.data;
+			});
+		});
+
+		app.controller('WorksController',function($scope, $http){
+			$http.get('js/trabalhos.json').then(function(res){
+			    $scope.trabalhoitens = res.data;
+			});
+		});
 	}]);
 
-	app.controller('ToolsController',function($scope, $http){
-		$http.get('js/ferramentas.json').then(function(res){
-		    $scope.ferramentaitens = res.data;
-		});
-	});
-
-	app.controller('WorksController',function($scope, $http){
-		$http.get('js/trabalhos.json').then(function(res){
-		    $scope.trabalhoitens = res.data;
-		});
-	});
+	
 })();
