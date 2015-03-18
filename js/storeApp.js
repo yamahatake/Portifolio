7711ac53dab66210a89400370d7eebce8f39ce4f
yamahatake <yamaaug@gmail.com>
2015-03-17 23:01:43 -0300

    .

--- js/storeApp.js
+++ js/storeApp.js
@@ -2,7 +2,7 @@
 
 	var app = angular.module("portfolio", ['ngRoute']);
 
-	app.config(['$routeProvider', '$httpProvider', function($routeProvider, $httpProvider) {
+	/*app.config(['$routeProvider', '$httpProvider', function($routeProvider, $httpProvider) {
 	    $httpProvider.defaults.cache = false;
 	    if (!$httpProvider.defaults.headers.get) {
 	      $httpProvider.defaults.headers.get = {};
@@ -10,18 +10,17 @@
 	    // disable IE ajax request caching
 	    $httpProvider.defaults.headers.get['If-Modified-Since'] = '0';
 	    //.....here proceed with your routes
-	    app.controller('ToolsController',function($scope, $http){
-			$http.get('js/ferramentas.json').then(function(res){
-			    $scope.ferramentaitens = res.data;
-			});
-		});
+	}]);*/
 
-		app.controller('WorksController',function($scope, $http){
-			$http.get('js/trabalhos.json').then(function(res){
-			    $scope.trabalhoitens = res.data;
-			});
+	app.controller('ToolsController',function($scope, $http){
+		$http.get('js/ferramentas.json').then(function(res){
+		    $scope.ferramentaitens = res.data;
 		});
-	}]);
+	});
 
-	
+	app.controller('WorksController',function($scope, $http){
+		$http.get('js/trabalhos.json').then(function(res){
+		    $scope.trabalhoitens = res.data;
+		});
+	});
 })();
