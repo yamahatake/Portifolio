307eaf4119d2c064ed40e859b77330d1e38ca98a
yamahatake <yamaaug@gmail.com>
2015-03-17 22:16:36 -0300

    teste ajax

--- js/storeApp.js
+++ js/storeApp.js
@@ -3,14 +3,32 @@
 	var app = angular.module("portfolio", []);
 
 	app.controller('ToolsController',function($scope, $http){
-		$http.get('js/ferramentas.json').then(function(res){
-		    $scope.ferramentaitens = res.data;                
-		});	 
+		/*$http.get('js/ferramentas.json').then(function(res){
+		    $scope.ferramentaitens = res.data;
+		});	*/
+		$.ajax({
+		    url: 'js/ferramentas.json',
+		    dataType: 'json',
+		    type: 'GET',
+		    crossDomain: true
+		    success: function(data){
+		        $scope.ferramentaitens = res.data;
+		    }
+		});
 	});
 
 	app.controller('WorksController',function($scope, $http){
-		$http.get('js/trabalhos.json').then(function(res){
-		    $scope.trabalhoitens = res.data;                
-		});	 
+		/*$http.get('js/trabalhos.json').then(function(res){
+		    $scope.trabalhoitens = res.data;
+		});	*/
+		$.ajax({
+		    url: 'js/ferramentas.json',
+		    dataType: 'json',
+		    type: 'GET',
+		    crossDomain: true
+		    success: function(data){
+		        $scope.ferramentaitens = res.data;
+		    }
+		});
 	});
 })();
