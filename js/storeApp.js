56b6b3714011e3571f8ffe4c3d98bed725c0e2f9
yamahatake <yamaaug@gmail.com>
2015-03-17 22:58:48 -0300

    .

--- js/storeApp.js
+++ js/storeApp.js
@@ -1,6 +1,16 @@
 (function(){
 
-	var app = angular.module("portfolio", []);
+	var app = angular.module("portfolio", ['ngRoute']);
+
+	app.config(['$routeProvider', '$httpProvider', function($routeProvider, $httpProvider) {
+	    $httpProvider.defaults.cache = false;
+	    if (!$httpProvider.defaults.headers.get) {
+	      $httpProvider.defaults.headers.get = {};
+	    }
+	    // disable IE ajax request caching
+	    $httpProvider.defaults.headers.get['If-Modified-Since'] = '0';
+	    //.....here proceed with your routes
+	}]);
 
 	app.controller('ToolsController',function($scope, $http){
 		$http.get('js/ferramentas.json').then(function(res){
