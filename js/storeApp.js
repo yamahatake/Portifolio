e3193fb645b477844c576b0c93347564b50f2893
yamahatake <yamaaug@gmail.com>
2015-03-17 23:06:28 -0300

    teste

--- js/storeApp.js
+++ js/storeApp.js
@@ -2,7 +2,7 @@
 
 	var app = angular.module("portfolio", []);
 
-	/*app.config(['$routeProvider', '$httpProvider', function($routeProvider, $httpProvider) {
+	app.config(['$routeProvider', '$httpProvider', function($routeProvider, $httpProvider) {
 	    $httpProvider.defaults.cache = false;
 	    if (!$httpProvider.defaults.headers.get) {
 	      $httpProvider.defaults.headers.get = {};
@@ -10,7 +10,7 @@
 	    // disable IE ajax request caching
 	    $httpProvider.defaults.headers.get['If-Modified-Since'] = '0';
 	    //.....here proceed with your routes
-	}]);*/
+	}]);
 
 	app.controller('ToolsController',function($scope, $http){
 		$http.get('js/ferramentas.json').then(function(res){
